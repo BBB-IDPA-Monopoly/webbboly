bb6737e5b255b7b7a2f13390a19211a2f4065081
Ricardo Cimma <ricardo.cimma@hayloft-it.ch>
2023-06-30 14:21:25 +0200

    Setup scss and finish ts setup

--- assets/app.ts
+++ assets/app.ts
@@ -1,4 +1,4 @@
-import './bootstrap.js';
+import './bootstrap';
 /*
  * Welcome to your app's main JavaScript file!
  *
@@ -7,4 +7,7 @@ import './bootstrap.js';
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import './styles/app.scss';
+
+// Bootstrap JS
+import 'bootstrap/dist/js/bootstrap.esm.min.js';
